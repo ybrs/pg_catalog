23b11ec8f0c6eade38b98c2ae5ed6c9eb78fa3ea
ybrs <aybars.badur@gmail.com>
2025-04-09 11:22:20 +0200

    parser

--- src/main.rs
+++ src/main.rs
@@ -1,28 +1,11 @@
-use datafusion::prelude::*;
+use sqlparser::ast::{BinaryOperator, Expr, ObjectName, SelectItem, SetExpr, Statement, TableAlias, TableFactor, TableWithJoins};
 use sqlparser::dialect::GenericDialect;
 use sqlparser::parser::Parser;
-use sqlparser::ast::{Statement, Query, SetExpr, Select, TableWithJoins, TableFactor, TableAlias, Expr, BinaryOperator, Value};
-use std::env;
 use std::collections::{HashMap, HashSet};
-
-
-fn process_table_factor(factor: &TableFactor, table_info: &mut HashMap<String, (HashSet<String>, HashSet<String>)>) {
-    match factor {
-        TableFactor::Table { name, .. } => {
-            let table_name = name.to_string();
-            table_info.entry(table_name).or_default();
-        }
-        TableFactor::Derived { subquery, alias, .. } => {
-            let table_name = alias.as_ref().map(|a| a.name.to_string()).unwrap_or_else(|| subquery.to_string());
-            table_info.entry(table_name).or_default();
-        }
-        _ => {}
-    }
-}
-
+use std::env;
 
 #[tokio::main]
-async fn main() -> datafusion::error::Result<()> {
+async fn main() {
     let args: Vec<String> = env::args().collect();
     if args.len() != 2 {
         eprintln!("Usage: {} \"SQL_QUERY\"", args[0]);
@@ -34,112 +17,134 @@ async fn main() -> datafusion::error::Result<()> {
     let ast = Parser::parse_sql(&dialect, sql_query).expect("Failed to parse SQL");
 
     let mut table_info: HashMap<String, (HashSet<String>, HashSet<String>)> = HashMap::new();
+    let mut aliases: HashMap<String, String> = HashMap::new();
 
     if let Some(Statement::Query(query)) = ast.get(0) {
         if let SetExpr::Select(select) = query.body.as_ref() {
-            process_select(select, &mut table_info);
+            process_select(select, &mut table_info, &mut aliases);
         }
     }
 
-    let result: Vec<_> = table_info.into_iter().map(|(table, (columns, filters))| {
-        serde_json::json!({
-            "table": table,
-            "columns": columns,
-            "filters": filters,
+    let result: Vec<_> = table_info
+        .into_iter()
+        .map(|(table, (columns, filters))| {
+            serde_json::json!({
+                "table": table,
+                "columns": columns,
+                "filters": filters,
+            })
         })
-    }).collect();
+        .collect();
 
     println!("{}", serde_json::to_string_pretty(&result).unwrap());
-
-    Ok(())
 }
 
-fn process_select(select: &Select, table_info: &mut HashMap<String, (HashSet<String>, HashSet<String>)>) {
+fn process_select(select: &sqlparser::ast::Select, table_info: &mut HashMap<String, (HashSet<String>, HashSet<String>)>, aliases: &mut HashMap<String, String>) {
     for table_with_joins in &select.from {
-        process_table_with_joins(table_with_joins, table_info);
+        process_table_with_joins(table_with_joins, table_info, aliases);
     }
 
     for projection in &select.projection {
-        if let sqlparser::ast::SelectItem::UnnamedExpr(expr) = projection {
-            process_expr(expr, table_info, true);
+        match projection {
+            SelectItem::UnnamedExpr(expr) | SelectItem::ExprWithAlias { expr, .. } => {
+                collect_columns(expr, table_info, aliases);
+            }
+            SelectItem::QualifiedWildcard(obj_name, _) => {
+                let table = resolve_table_name(&obj_name.to_string(), aliases);
+                table_info.entry(table).or_default();
+            }
+            SelectItem::Wildcard(_) => {}
         }
     }
 
     if let Some(selection) = &select.selection {
-        process_expr(selection, table_info, false);
+        distribute_filters(selection, table_info, aliases);
     }
 }
 
-fn process_table_with_joins(table_with_joins: &TableWithJoins, table_info: &mut HashMap<String, (HashSet<String>, HashSet<String>)>) {
-    let table_name = match &table_with_joins.relation {
-        TableFactor::Table { name, .. } => name.to_string(),
-        TableFactor::Derived { subquery, alias, .. } => {
-            if let Some(alias) = alias {
-                alias.name.to_string()
-            } else {
-                subquery.to_string()
-            }
-        }
-        _ => return,
-    };
-
-    table_info.entry(table_name.clone()).or_default();
-
-    for join in &table_with_joins.joins {
-        process_table_factor(&join.relation, table_info);
-
-        match &join.join_operator {
-            sqlparser::ast::JoinOperator::Inner(constraint)
-            | sqlparser::ast::JoinOperator::LeftOuter(constraint)
-            | sqlparser::ast::JoinOperator::RightOuter(constraint)
-            | sqlparser::ast::JoinOperator::FullOuter(constraint)
-            | sqlparser::ast::JoinOperator::LeftAnti(constraint)
-            | sqlparser::ast::JoinOperator::LeftSemi(constraint)
-            | sqlparser::ast::JoinOperator::RightAnti(constraint)
-            | sqlparser::ast::JoinOperator::RightSemi(constraint) => {
-                if let sqlparser::ast::JoinConstraint::On(expr) = constraint {
-                    process_expr(expr, table_info, false);
-                }
-            }
-            sqlparser::ast::JoinOperator::CrossJoin
-            | sqlparser::ast::JoinOperator::CrossApply
-            | sqlparser::ast::JoinOperator::OuterApply => {
-                // no ON condition to process
-            }
-            _ => {}
-        }
-    }
-}
-
-fn process_expr(expr: &Expr, table_info: &mut HashMap<String, (HashSet<String>, HashSet<String>)>, is_projection: bool) {
+fn collect_columns(expr: &Expr, table_info: &mut HashMap<String, (HashSet<String>, HashSet<String>)>, aliases: &HashMap<String, String>) {
     match expr {
+        Expr::CompoundIdentifier(idents) if idents.len() == 2 => {
+            let table = resolve_table_name(&idents[0].value, aliases);
+            let column = &idents[1].value;
+            table_info.entry(table).or_default().0.insert(column.clone());
+        }
         Expr::Identifier(ident) => {
-            if is_projection {
-                for (table, (columns, _)) in table_info.iter_mut() {
-                    columns.insert(ident.value.clone());
-                }
+            for (_table, (cols, _)) in table_info.iter_mut() {
+                cols.insert(ident.value.clone());
             }
         }
-        Expr::CompoundIdentifier(idents) => {
-            if idents.len() == 2 {
-                let table = &idents[0].value;
-                let column = &idents[1].value;
-                if let Some((columns, _)) = table_info.get_mut(table) {
-                    columns.insert(column.clone());
-                }
-            }
+        Expr::BinaryOp { left, right, .. } => {
+            collect_columns(left, table_info, aliases);
+            collect_columns(right, table_info, aliases);
         }
-        Expr::BinaryOp { left, op, right } => {
-            if !is_projection {
-                let filter = format!("{} {} {}", left, op, right);
-                for (table, (_, filters)) in table_info.iter_mut() {
-                    filters.insert(filter.clone());
-                }
-            }
-            process_expr(left, table_info, is_projection);
-            process_expr(right, table_info, is_projection);
-        }
-        Expr::Nested(expr) => process_expr(expr, table_info, is_projection),
+        Expr::Nested(e) => collect_columns(e, table_info, aliases),
         _ => {}
     }
+}
+
+fn distribute_filters(expr: &Expr, table_info: &mut HashMap<String, (HashSet<String>, HashSet<String>)>, aliases: &HashMap<String, String>) {
+    match expr {
+        Expr::BinaryOp { left, op, right } if matches!(op, BinaryOperator::And) => {
+            distribute_filters(left, table_info, aliases);
+            distribute_filters(right, table_info, aliases);
+        }
+        Expr::BinaryOp { .. } => {
+            let involved_tables: HashSet<_> = extract_tables_from_expr(expr, aliases);
+            if involved_tables.len() == 1 {
+                if let Some(table) = involved_tables.iter().next() {
+                    table_info.entry(table.clone()).or_default().1.insert(expr.to_string());
+                }
+            }
+        }
+        Expr::Nested(e) => distribute_filters(e, table_info, aliases),
+        _ => {}
+    }
+}
+
+fn extract_tables_from_expr(expr: &Expr, aliases: &HashMap<String, String>) -> HashSet<String> {
+    let mut set = HashSet::new();
+    match expr {
+        Expr::CompoundIdentifier(idents) if idents.len() == 2 => {
+            let table = resolve_table_name(&idents[0].value, aliases);
+            set.insert(table);
+        }
+        Expr::BinaryOp { left, right, .. } => {
+            set.extend(extract_tables_from_expr(left, aliases));
+            set.extend(extract_tables_from_expr(right, aliases));
+        }
+        Expr::Nested(e) => {
+            set.extend(extract_tables_from_expr(e, aliases));
+        }
+        _ => {}
+    }
+    set
+}
+
+fn process_table_with_joins(table_with_joins: &TableWithJoins, table_info: &mut HashMap<String, (HashSet<String>, HashSet<String>)>, aliases: &mut HashMap<String, String>) {
+    process_table_factor(&table_with_joins.relation, table_info, aliases);
+    for join in &table_with_joins.joins {
+        process_table_factor(&join.relation, table_info, aliases);
+    }
+}
+
+fn process_table_factor(factor: &TableFactor, table_info: &mut HashMap<String, (HashSet<String>, HashSet<String>)>, aliases: &mut HashMap<String, String>) {
+    match factor {
+        TableFactor::Table { name, alias, .. } => {
+            let real = name.to_string();
+            table_info.entry(real.clone()).or_default();
+            if let Some(TableAlias { name: alias_name, .. }) = alias {
+                aliases.insert(alias_name.value.clone(), real);
+            }
+        }
+        TableFactor::Derived { subquery, alias, .. } => {
+            let table = alias.as_ref().map(|a| a.name.to_string()).unwrap_or_else(|| subquery.to_string());
+            table_info.entry(table).or_default();
+        }
+        _ => {}
+    }
+}
+
+fn resolve_table_name(alias_or_name: &str, aliases: &HashMap<String, String>) -> String {
+    aliases.get(alias_or_name).cloned().unwrap_or_else(|| alias_or_name.to_string())
 }
