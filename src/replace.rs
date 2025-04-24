2fc487d047d7d3bace5592fe2b7db19e59e66d95
Aybars Badur <aybars.badur@gmail.com>
2025-04-24 15:41:54 +0200

    wip

--- src/replace.rs
+++ src/replace.rs
@@ -38,22 +38,65 @@ pub fn create_regclass_udf() -> ScalarUDF {
 use sqlparser::ast::*;
 
 
+// pub fn parse_sql_and_dump_ast(sql: &str) {
+//     let dialect = PostgreSqlDialect {};
+//     let mut statements = Parser::parse_sql(&dialect, sql).unwrap();
+//
+//     visit_statements_mut(&mut statements, |stmt| {
+//         if let Statement::Query(query) = stmt {
+//             query.limit = None;
+//         }
+//         ControlFlow::<()>::Continue(())
+//     });
+//
+//     for stmt in statements {
+//         println!("{}", stmt);
+//     }
+// }
+
+
 pub fn parse_sql_and_dump_ast(sql: &str) {
     let dialect = PostgreSqlDialect {};
     let mut statements = Parser::parse_sql(&dialect, sql).unwrap();
 
-    visit_statements_mut(&mut statements, |stmt| {
-        if let Statement::Query(query) = stmt {
-            query.limit = None;
-        }
-        ControlFlow::<()>::Continue(())
 
+    visit_statements_mut(&mut statements, |stmt| {
+        visit_expressions_mut(stmt, |expr| {
+            dbg!(&expr); // leave this if you want to debug input
+            if let Expr::Cast { data_type, .. } = expr {
+                if let DataType::Regclass = data_type {
+                    *expr = Expr::Function(Function {
+                        name: ObjectName(vec![ObjectNamePart::Identifier(Ident::new("regclass"))]),
+                        over: None,
+                        filter: None,
+                        within_group: vec![],
+                        null_treatment: None,
+                        uses_odbc_syntax: false,
+                        parameters: FunctionArguments::None,
+                        args: FunctionArguments::List(FunctionArgumentList {
+                            duplicate_treatment: None,
+                            args: vec![
+                                FunctionArg::Unnamed(FunctionArgExpr::Expr(
+                                    Expr::Value(Value::SingleQuotedString("foo".into()).into())
+                                ))
+                            ],
+                            clauses: vec![],
+                        }),
+                    });
+                }
+            }
+            ControlFlow::Continue(())
+        })?;
+        ControlFlow::<()>::Continue(())
     });
 
+    println!("sql after ====");
     for stmt in statements {
         println!("{}", stmt);
     }
+    println!("=======");
 }
+
 
 #[cfg(test)]
 mod tests {
@@ -64,7 +107,7 @@ mod tests {
     #[test]
     fn test_usage() -> Result<(), Box<dyn Error>> {
 
-        parse_sql_and_dump_ast("select * from foo limit 10");
+        parse_sql_and_dump_ast("select 'pg_namespace'::regclass from foo limit 10");
         Ok(())
     }
 }
