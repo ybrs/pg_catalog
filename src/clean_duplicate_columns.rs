e9d2a9e62942c307e58f7eb1fff7fa384229bf33
Aybars Badur <aybars.badur@gmail.com>
2025-04-25 11:59:11 +0200

    works with subselects

--- src/clean_duplicate_columns.rs
+++ src/clean_duplicate_columns.rs
@@ -6,62 +6,76 @@ use std::ops::ControlFlow;
 use uuid::Uuid;
 
 
+fn alias_projection(select: &mut Select) {
+    let mut new_proj = Vec::new();
+    for item in &select.projection {
+        match item {
+            SelectItem::UnnamedExpr(expr) => match expr {
+                Expr::Wildcard(_) | Expr::QualifiedWildcard(_, _) => {
+                    new_proj.push(SelectItem::UnnamedExpr(expr.clone()));
+                }
+                _ => {
+                    let alias = format!("\"{}\"", Uuid::new_v4().simple());
+                    new_proj.push(SelectItem::ExprWithAlias {
+                        expr: expr.clone(),
+                        alias: Ident::new(alias),
+                    });
+                }
+            },
+            _ => new_proj.push(item.clone()),
+        }
+    }
+    select.projection = new_proj;
+}
+
+fn walk_set_expr(expr: &mut SetExpr) {
+    match expr {
+        SetExpr::Select(select) => {
+            alias_projection(select);
+        }
+        SetExpr::SetOperation { left, right, .. } => {
+            walk_set_expr(left);
+            walk_set_expr(right);
+        }
+        SetExpr::Query(subquery) => {
+            walk_query(subquery);
+        }
+        _ => {}
+    }
+}
+
+fn walk_query(query: &mut Query) {
+    walk_set_expr(&mut query.body);
+
+    if let Some(with) = &mut query.with {
+        for cte in &mut with.cte_tables {
+            walk_query(&mut cte.query);
+        }
+    }
+}
+
 pub fn alias_all_columns(sql: &str) -> String {
     let dialect = PostgreSqlDialect {};
     let mut statements = Parser::parse_sql(&dialect, sql).unwrap();
 
     let _ = visit_statements_mut(&mut statements, |stmt| {
         if let Statement::Query(query) = stmt {
-            let mut stack = vec![&mut query.body];
-
-            while let Some(set_expr) = stack.pop() {
-                match set_expr.as_mut() {
-                    SetExpr::Select(select) => {
-                        let mut new_proj = Vec::new();
-                        for item in &select.projection {
-                            match item {
-                                SelectItem::UnnamedExpr(expr) => match expr {
-                                    Expr::Wildcard(_) | Expr::QualifiedWildcard(_, _) => {
-                                        new_proj.push(SelectItem::UnnamedExpr(expr.clone()));
-                                    }
-                                    _ => {
-                                        let alias = format!("\"{}\"", Uuid::new_v4().simple());
-                                        new_proj.push(SelectItem::ExprWithAlias {
-                                            expr: expr.clone(),
-                                            alias: Ident::new(alias),
-                                        });
-                                    }
-                                },
-                                _ => new_proj.push(item.clone()),
-                            }
-                        }
-                        select.projection = new_proj;
-                    }
-                    SetExpr::SetOperation { left, right, .. } => {
-                        stack.push(left);
-                        stack.push(right);
-                    }
-                    SetExpr::Query(q) => {
-                        stack.push(&mut q.body);
-                    }
-                    _ => {}
-                }
-            }
+            walk_query(query);
         }
-
         ControlFlow::<()>::Continue(())
     });
 
-
-    let result = statements
+    let res = statements
         .into_iter()
         .map(|stmt| stmt.to_string())
         .collect::<Vec<_>>()
         .join(" ");
 
-    println!("{}", result);
-    result
+    println!("result: {:?}", res);
+
+    res
 }
+
 
 #[cfg(test)]
 mod tests {
@@ -97,12 +111,13 @@ mod tests {
             ),
 
             (
-                "select * from (SELECT t.a FROM t) T1",
-                vec!["SELECT t.a AS", ""], // TODO: fix the test case
-            ),
-            (
                 "WITH cte AS (SELECT t.a FROM t) SELECT * FROM cte",
                 vec!["SELECT t.a AS", "SELECT * FROM cte"], // t.a gets alias, SELECT * stays
+            ),
+
+            (
+                "select * from (SELECT t.a FROM t) T1",
+                vec!["SELECT t.a AS", ""], // TODO: fix the test case
             ),
         ];
 
